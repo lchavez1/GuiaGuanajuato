75075a148c9bb0c091bb35bb7688ca3577630032
Luis <Luisferchs40@gmial.com>
2020-06-02 23:57:52 -0500

    actualizacion repo

--- GuiaGuanajuato/GuiaGuanajuato/GuiaGuanajuato.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ GuiaGuanajuato/GuiaGuanajuato/GuiaGuanajuato.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"044780e7-3d01-47a6-9930-d43c8d2b1bb6"
+	.asciz	"d94b0963-232e-493d-ac4c-091fe0f8d4d6"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
